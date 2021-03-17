3533f42d40801a7b239d648a63dcc9f16dbeb8b0
Cristian Alarcón <calarconpieriz@gmail.com>
2021-03-17 02:02:55 -0300

    model updated

--- src/domain/entities/UserAccount.ts
+++ src/domain/entities/UserAccount.ts
@@ -1,28 +1,82 @@
+interface Session {
+  device: string;
+  deviceOS: string;
+  deviceId: string;
+  appVersion: string;
+}
+
+interface Bank {
+  accountNumber: string;
+  name: string;
+}
+
+interface Withdrawal {
+  type: string;
+  amount: number;
+  status: string;
+  createdAt: Date;
+  paidAt: Date;
+}
+
+interface Advance {
+  amount: number;
+  day: number;
+  withdrawals: Withdrawal[];
+}
+
 export class UserAccount {
-  private name: string;
-  private url: string;
+  private userId: string;
+  private documentNumber: string;
+  private firstName: string;
+  private lastName: string;
+  private email: string;
+  private phoneNumber: string;
+  private companyName: string;
+  private session?: Session;
+  private salary: number;
+  private bank?: Bank;
+  private withdrawals?: Withdrawal[];
+  private advances?: Advance[];
   constructor() {
-    this.name = '';
-    this.url = '';
+    this.userId = '';
+    this.documentNumber = '';
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.phoneNumber = '';
+    this.companyName = '';
+    this.salary = 0;
   }
 
-  public getName(): string {
-    return this.name;
+  public getUserId(): string {
+    return this.userId;
   }
 
-  public getUrl(): string {
-    return this.url;
+  public getDocumentNumber(): string {
+    return this.documentNumber;
   }
 
-  public setName(name: string): void {
-    this.name = name;
+  public getFirstName(): string {
+    return this.firstName;
   }
 
-  public setUrl(url: string): void {
-    this.url = url;
+  public getLastName(): string {
+    return this.lastName;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setUserId(userId: string): void {
+    this.userId = userId;
+  }
+
+  public setDocumentNumber(documentNumber: string): void {
+    this.documentNumber = documentNumber;
   }
 }
 
 export interface FetchUserAccountSuccessResponse {
-  userAccount: any;
+  userAccount: UserAccount;
 }

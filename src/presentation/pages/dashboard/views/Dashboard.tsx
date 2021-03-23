5c2b98c7656fca466a676b131f59d8dc5a62d44b
Cristian Alarcón <calarconpieriz@gmail.com>
2021-03-23 03:31:35 -0300

    Dashboard view

---
+++ src/presentation/pages/dashboard/views/Dashboard.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import Divider from '@material-ui/core/Divider';
+import { connect } from 'react-redux';
+import { fetchData } from '../store/dashboard.actions';
+// Components
+import { Account, Profile, Transactions } from '../components';
+import { State as DashboardState } from '../store/dashboard.reducers';
+import { LinearProgress } from '@material-ui/core';
+interface Props {
+  state: DashboardState;
+  fetchData: any;
+}
+
+const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding-top: 10px;
+`;
+
+const ProgressBarContainer = styled.div`
+  width: 100%;
+`;
+
+const Dashboard = ({ state, fetchData }: Props): any => {
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+  const { userAccount, loading } = state;
+  const { withdrawals } = userAccount;
+  console.log(userAccount);
+  const progressBar = () => {
+    return (
+      <ProgressBarContainer>
+        <LinearProgress color="secondary" />
+      </ProgressBarContainer>
+    );
+  };
+  const content = () => {
+    return (
+      <DashboardContainer>
+        <Profile userAccount={userAccount} />
+        <Account userAccount={userAccount} />
+        <Divider />
+        <Transactions withdrawals={withdrawals} />
+      </DashboardContainer>
+    );
+  };
+  return loading ? progressBar() : content();
+};
+
+const mapStateToProps = (state: any) => ({
+  state: state.dashboard,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchData: () => dispatch(fetchData),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);

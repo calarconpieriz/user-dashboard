287ebab74a6bdb19da9471746bc3475e46da2c02
Cristian Alarcón <calarconpieriz@gmail.com>
2021-03-23 03:28:45 -0300

    Redux devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css';
-import { Dashboard } from './pages/dashboard/views';
+import { Dashboard } from './presentation/pages/dashboard/views';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import dashboardReducer from './pages/dashboard/store/dashboard.reducers';
-import Navbar from './shared/components/Navbar';
+import dashboard from './presentation/pages/dashboard/store/dashboard.reducers';
+import Navbar from './presentation/shared/components/Navbar';
 import { createMuiTheme } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducers = combineReducers({ dashboardReducer });
-const store = createStore(reducers, applyMiddleware(thunk));
+const reducers = combineReducers({ dashboard });
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 const theme = createMuiTheme({
   palette: {

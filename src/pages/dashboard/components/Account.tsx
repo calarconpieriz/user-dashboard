9661b79151185bdd0ca0a79f499d0f8d3a3c7223
Cristian Alarcón <calarconpieriz@gmail.com>
2021-03-23 03:30:23 -0300

    Files moved

--- src/pages/dashboard/components/Account.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Account = () => {
-  return <div className="account"></div>;
-};
-
-export default Account;
--- src/pages/dashboard/store/dashboard.types.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Fetches a dashboard data
- */
-export const FETCH_DATA = '[Dashboard] Fecth data';
-export const FETCH_DATA_SUCCESS = '[Dashboard] Fecth data success';
-export const FETCH_DATA_FAILURE = '[Dashboard] Fecth data failure';
